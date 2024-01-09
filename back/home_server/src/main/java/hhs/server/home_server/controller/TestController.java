0127b365756f26d0a28a33a098aeaae859a828dc
heesane <tees3359@gmail.com>
2024-01-09 18:13:34 +0900

    feat : FileUpload 기능 구현 중

--- back/home_server/src/main/java/hhs/server/home_server/controller/TestController.java
+++ back/home_server/src/main/java/hhs/server/home_server/controller/TestController.java
@@ -1,4 +1,33 @@
 package hhs.server.home_server.controller;
 
-public class TestController {
+import hhs.server.home_server.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/files")
+public class TestController{
+
+    @Autowired
+    private TestService fileUploadService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            // 파일 업로드 서비스 호출
+            fileUploadService.uploadFile(file);
+
+            return ResponseEntity.ok("File uploaded successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Error uploading the file");
+        }
+    }
 }
