5a849a51a8a77b5fae569b9d95296b9d7784583d
heesane <tees3359@gmail.com>
2024-02-26 04:51:58 +0900

    feat : Lowbar and Topbar

---
+++ front/app/components/Topbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function Topbar() {
+  return (
+    <header className="border-b">
+        <div className="container px-4 lg:px-6 h-14 flex items-center">
+          <Link className="flex items-center justify-center text-2xl" href="/.">
+            <span className="font-semibold">HeeSang Hwang</span>
+          </Link>
+          <nav className="ml-auto flex gap-4 sm:gap-6">
+            <Link className="text-m font-medium hover:underline underline-offset-4" href="/projects">
+              Projects
+            </Link>
+            <Link className="text-m font-medium hover:underline underline-offset-4" href="/about">
+              About Me
+            </Link>
+            <Link className="text-m font-medium hover:underline underline-offset-4" href="/contacts">
+              Contact
+            </Link>
+          </nav>
+        </div>
+      </header>
+  );
+}
