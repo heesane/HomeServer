2ae9c9baaca0db396b3594139def56616acc9899
heesane <tees3359@gmail.com>
2024-02-26 04:53:39 +0900

    feat : pagination 추가

---
+++ front/app/components/ui/pagination.tsx
@@ -0,0 +1,31 @@
+// Pagination.tsx
+import React from 'react';
+
+export function Pagination() {
+  return (
+    <nav aria-label="Pagination">
+      <ul className="flex space-x-2">
+        <li>
+          <a className="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" href="#">
+            Previous
+          </a>
+        </li>
+        <li>
+          <a className="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" href="#">
+            1
+          </a>
+        </li>
+        <li>
+          <a className="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" href="#">
+            2
+          </a>
+        </li>
+        <li>
+          <a className="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" href="#">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
