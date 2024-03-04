acfe3d426f22c08750e854939a68df1ec10fdcbd
heesane <tees3359@gmail.com>
2024-03-05 03:42:26 +0900

    feat : 구현

--- back/home_server/src/main/java/hhs/server/home_server/HomeServerApplication.java
+++ back/home_server/src/main/java/hhs/server/home_server/HomeServerApplication.java
@@ -1,16 +1,29 @@
 package hhs.server.home_server;
 
 import hhs.server.home_server.config.CorsConfig;
+import hhs.server.home_server.properties.StorageProperties;
+import hhs.server.home_server.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(CorsConfig.class)
+@EnableConfigurationProperties(StorageProperties.class)
 public class HomeServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeServerApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
---
+++ back/home_server/src/main/java/hhs/server/home_server/exception/StorageException.java
@@ -0,0 +1,10 @@
+package hhs.server.home_server.exception;
+
+public class StorageException extends RuntimeException{
+    public StorageException(String message){
+        super(message);
+    }
+    public StorageException(String message,Throwable cause){
+        super(message,cause);
+    }
+}
