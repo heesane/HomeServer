9151d63ce6932633ad4935609d3beddf20c2d3d8
heesane <tees3359@gmail.com>
2024-03-13 22:51:45 +0900

    feat/front : portfolio 페이지 작성 중

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,10 @@
-// 메인 페이지
+import Portfolio from './portpolio';
 
-
+// 메인 페이지 - 포트폴리오를 보여주는 페이지
 export default function Home() {
   return (
     <div>
-      
+      <Portfolio/>
     </div>
   );
 }
