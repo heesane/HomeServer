203155401d46fc7c746ac4ba1af6841a03d216ce
heesane <tees3359@gmail.com>
2024-03-17 18:59:16 +0900

    back/feat : python dotenv

--- back/fastapi/database.py
+++ back/fastapi/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://s3_admin:002677005k@mysql:3306/s3?charset=utf8mb4"
+load_dotenv()
 
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
