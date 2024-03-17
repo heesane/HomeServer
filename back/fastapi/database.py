3c63b2bd49f8a656f43d2e5463aa3f516d222c69
heesane <tees3359@gmail.com>
2024-03-17 18:09:24 +0900

    back/fix : DB URL

--- back/fastapi/database.py
+++ back/fastapi/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://s3:267700@localhost:3306/s3?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://s3_admin:002677005k@mysql:3306/s3?charset=utf8mb4"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
