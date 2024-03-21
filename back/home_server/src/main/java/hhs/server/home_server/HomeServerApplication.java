4d0e0419564a691d7b47527eae4b427e698f44c3
heesane <tees3359@gmail.com>
2024-03-21 16:44:07 +0900

    back/feat : @ConfigurationProperties 삭제

--- back/home_server/src/main/java/hhs/server/home_server/HomeServerApplication.java
+++ back/home_server/src/main/java/hhs/server/home_server/HomeServerApplication.java
@@ -6,7 +6,6 @@ import hhs.server.home_server.service.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
