000ffe709e04db3014c588eabdc019c431109cf6
heesane <tees3359@gmail.com>
2024-07-11 03:50:20 +0900

    Shell Script 최신화

---
+++ stop-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Author : Heesane
+# Description: This script will run the Docker Container
+
+FILE_PATH="/home/$USER/server"
+
+sudo docker compose -f "$FILE_PATH/back/docker-compose.back.yml" down
+sudo docker compose -f "$FILE_PATH/front/docker-compose.front.yml" down
+sudo docker compose -f "$FILE_PATH/devops/docker-compose.monitoring.yml" down
+sudo docker compose -f "$FILE_PATH/mq/docker-compose.mq.yml" down
+sudo docker compose -f "$FILE_PATH/database/docker-compose.database.yml" down
