8191e1519f0b7e9e2735acf27654d8a788008a5e
heesane <tees3359@gmail.com>
2024-07-11 21:32:09 +0900

    remove unused import

--- back/home_server/src/main/java/hhs/server/home_server/config/CorsConfig.java
+++ back/home_server/src/main/java/hhs/server/home_server/config/CorsConfig.java
@@ -2,9 +2,6 @@ package hhs.server.home_server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://api.heesang.pro","http://localhost:3000","https://localhost:3000")
+                        .allowedOrigins("https://api.heesang.pro", "http://localhost:3000", "https://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true)
                         .maxAge(3600);
