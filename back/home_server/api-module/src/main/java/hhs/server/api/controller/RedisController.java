43ce9a130b774bcbe9e76dbe8da385b4048ffa51
heesane <tees3359@gmail.com>
2024-07-27 02:28:30 +0900

    [API] fix: 수정 예정

--- back/home_server/api-module/src/main/java/hhs/server/api/controller/RedisController.java
+++ back/home_server/api-module/src/main/java/hhs/server/api/controller/RedisController.java
@@ -1,20 +1,17 @@
 package hhs.server.api.controller;
 
 import hhs.server.api.service.RedisService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class RedisController {
-    private final RedisService redisService;
 
-    @Autowired
-    public RedisController(RedisService redisService){
-        this.redisService = redisService;
-    }
+    private final RedisService redisService;
 
     @PostMapping("/redis/save")
     public String saveData(@RequestParam String key, @RequestParam String value){
