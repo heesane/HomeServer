6bc15926511157cbe7defbbba90ed3d6e6d57f0c
heesane <tees3359@gmail.com>
2024-07-27 02:24:53 +0900

    [API] @Component Scan

--- back/home_server/api-module/src/main/java/hhs/server/api/ApiApplication.java
+++ back/home_server/api-module/src/main/java/hhs/server/api/ApiApplication.java
@@ -3,9 +3,14 @@ package hhs.server.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = {"hhs.server.domain", "hhs.server.common"})
-@SpringBootApplication(scanBasePackages = {"hhs.server"})
+
+@ComponentScan(basePackages = "hhs.server")
+@EntityScan(basePackages = "hhs.server.domain")
+@EnableJpaRepositories(basePackages = "hhs.server.domain.repository")
+@SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
