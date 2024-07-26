0a91f1e77dd6609b7551248d0b646bb6b2992e19
heesane <tees3359@gmail.com>
2024-07-27 02:25:23 +0900

    [API] fix: BcryptPasswordEncoderConfig Bean

---
+++ back/home_server/api-module/src/main/java/hhs/server/api/config/BcryptPasswordEncoderConfig.java
@@ -0,0 +1,13 @@
+package hhs.server.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BcryptPasswordEncoderConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
