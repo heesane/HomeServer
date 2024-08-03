834bc03fc5320e8038ddb951cc2b6687f6e9dde7
heesane <tees3359@gmail.com>
2024-08-04 02:04:24 +0900

    각 모듈별 log 추가

--- back/home_server/batch-module/src/main/java/hhs/server/batch/BatchApplication.java
+++ back/home_server/batch-module/src/main/java/hhs/server/batch/BatchApplication.java
@@ -1,13 +1,18 @@
 package hhs.server.batch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@Slf4j
 @EntityScan(basePackages = {"hhs.server.domain", "hhs.server.common"})
 @SpringBootApplication(scanBasePackages = "hhs.server")
 public class BatchApplication {
     public static void main(String[] args) {
+        MDC.put("moduleName","BATCH");
+        log.info("Starting Batch module");
         SpringApplication.run(BatchApplication.class, args);
     }
 }
