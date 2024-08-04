625ea1b0c400700dce3d2486c13f7b6efc361951
heesane <tees3359@gmail.com>
2024-08-04 22:53:54 +0900

    [SERVER] docker script

--- stop-docker.sh
+++ stop-docker.sh
@@ -10,3 +10,4 @@ sudo docker compose -f "$FILE_PATH/front/docker-compose.front.yml" down
 sudo docker compose -f "$FILE_PATH/devops/docker-compose.monitoring.yml" down
 sudo docker compose -f "$FILE_PATH/mq/docker-compose.mq.yml" down
 sudo docker compose -f "$FILE_PATH/database/docker-compose.database.yml" down
+sudo docker compose -f "$FILE_PATH/devops/docker-compose.devops.yml" down
