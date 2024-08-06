e214f407a5c5d58d97de56b99647cb7d21b337b0
heesane <tees3359@gmail.com>
2024-08-06 19:19:59 +0900

    [SERVER] Script

--- start-docker.sh
+++ start-docker.sh
@@ -6,7 +6,6 @@
 FILE_PATH="/home/$USER/server"
 
 sudo docker compose -f "$FILE_PATH/back/docker-compose.back.yml" up -d --build
-sudo docker compose -f "$FILE_PATH/front/docker-compose.front.yml" up -d --build
-sudo docker compose -f "$FILE_PATH/devops/docker-compose.monitoring.yml" up -d --build
 sudo docker compose -f "$FILE_PATH/mq/docker-compose.mq.yml" up -d --build
 sudo docker compose -f "$FILE_PATH/database/docker-compose.database.yml" up -d --build
+sudo docker compose -f "$FILE_PATH/devops/docker-compose.devops.yml" up -d --build
--- stop-docker.sh
+++ stop-docker.sh
@@ -6,8 +6,6 @@
 FILE_PATH="/home/$USER/server"
 
 sudo docker compose -f "$FILE_PATH/back/docker-compose.back.yml" down
-sudo docker compose -f "$FILE_PATH/front/docker-compose.front.yml" down
-sudo docker compose -f "$FILE_PATH/devops/docker-compose.monitoring.yml" down
 sudo docker compose -f "$FILE_PATH/mq/docker-compose.mq.yml" down
 sudo docker compose -f "$FILE_PATH/database/docker-compose.database.yml" down
 sudo docker compose -f "$FILE_PATH/devops/docker-compose.devops.yml" down
