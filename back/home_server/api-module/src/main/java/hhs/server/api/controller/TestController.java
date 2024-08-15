a7ebb04768a4e078f61120a98321bbdc930be0ee
heesane <tees3359@gmail.com>
2024-08-15 14:45:22 +0900

    [LOCAL][API] 예시 controller & service

---
+++ back/home_server/api-module/src/main/java/hhs/server/api/controller/TestController.java
@@ -0,0 +1,33 @@
+package hhs.server.api.controller;
+
+import com.google.protobuf.Message;
+import hhs.server.api.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class TestController {
+
+    private final TestService testService;
+
+    @GetMapping("/grpc/test")
+    public String testGRPC() {
+        Map<String, Object> result = new HashMap<>();
+        String response = testService.Testing("hello");
+        result.put("Hello", response);
+
+        // in header
+        // Accept-Language: en-US
+        // Accept-Language: ko
+        Locale currentLocale = LocaleContextHolder.getLocale();
+        return response;
+
+    }
+}
