f40cbd44fd13adb7671ff6fbc6de72cb7b82de2e
heesane <tees3359@gmail.com>
2024-08-17 15:47:59 +0900

    [LOCAL] 심심할땐 Next.js

---
+++ next/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default function About(){
+    return (
+        <div className="container py-8">
+        <h1 className="text-3xl font-bold">About</h1>
+        <p className="mt-4 text-lg">Coming soon...</p>
+        </div>
+    );
+}
---
+++ next/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Contact() {
+    return (
+        <div className="container py-8">
+        <h1 className="text-3xl font-bold">Contact</h1>
+        <p className="mt-4 text-lg">Coming soon...</p>
+        </div>
+    );
+}
