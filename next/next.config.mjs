932c086d8875ffc4dcbfa94fd604cc1c9457f36c
heesane <tees3359@gmail.com>
2024-08-20 23:43:53 +0900

    [SERVER] images remote patterns

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["avatars.githubusercontent.com"]
-    }
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname:'/**'
+            },
+            {
+                protocol: 'http',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname:'/**'
+            },
+        ],
+    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: "/:path*",
+    //             destination: "(전달받은 API 주소)/:path*",
+    //         }
+    //     ]
+    // }
 };
 
 export default nextConfig;
