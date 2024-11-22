b60196bb890e9fe62c41433150a303792a2df1d8
heesane <tees3359@gmail.com>
2024-11-23 01:16:53 +0900

    refactor : About Page CSS

--- next/src/app/about/page.tsx
+++ next/src/app/about/page.tsx
@@ -1,9 +1,15 @@
 
 export default function About(){
     return (
-        <div className="container py-8">
-        <h1 className="text-3xl font-bold">About</h1>
-        <p className="mt-4 text-lg">Coming soon...</p>
+      <div className="container mx-auto py-14 px-4 sm:px-6 lg:px-8 justify-between">
+        <div className="text-2xl font-bold sm:text-4xl sm:px-6 lg:px-8 px-4 justify-between">
+          <div className="px-4 sm:px-6 lg:px-8">
+            <h1>About</h1>
+
+            <p className="mt-4 text-lg">Coming soon...</p>
+          </div>
+
         </div>
+      </div>
     );
 }
