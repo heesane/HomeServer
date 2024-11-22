c4bfa7dce8e5f4c4a94cb624f12888fff3041a25
heesane <tees3359@gmail.com>
2024-11-23 01:31:52 +0900

    refactor : project component 규격

--- next/src/app/projects/[id]/page.tsx
+++ next/src/app/projects/[id]/page.tsx
@@ -3,10 +3,14 @@ import { ProjectId } from '@/type/interface'
 
 export default function Home({params}: {params: ProjectId}) {
   return (
-    <div>
-      <h1>
-        {params.id}
-      </h1>
+    <div className="container mx-auto py-14 px-4 sm:px-6 lg:px-8 justify-between">
+      <div className="text-2xl font-bold sm:text-4xl sm:px-6 lg:px-8 px-4 justify-between">
+        <div className="px-4 sm:px-6 lg:px-8">
+          <h1>{params.id}</h1>
+
+          <p className="mt-4 text-lg">Coming soon...</p>
+        </div>
+      </div>
     </div>
   )
 }
