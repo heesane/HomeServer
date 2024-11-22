7fd380f6371a5b834ffcb231378d209ef9837b52
heesane <tees3359@gmail.com>
2024-11-23 01:17:00 +0900

    refactor : Contact Page CSS

--- next/src/app/contact/page.tsx
+++ next/src/app/contact/page.tsx
@@ -1,9 +1,14 @@
 
 export default function Contact() {
     return (
-        <div className="container py-8">
-        <h1 className="text-3xl font-bold">Contact</h1>
-        <p className="mt-4 text-lg">Coming soon...</p>
+      <div className="container mx-auto py-14 px-4 sm:px-6 lg:px-8 justify-between">
+        <div className="text-2xl font-bold sm:text-4xl sm:px-6 lg:px-8 px-4 justify-between">
+          <div className="px-4 sm:px-6 lg:px-8">
+            <h1>Contact</h1>
+
+            <p className="mt-4 text-lg">Coming soon...</p>
+          </div>
         </div>
+      </div>
     );
 }
