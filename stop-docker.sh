398a37bdab8c0f5e5d107b851a68bc9142ca7885
heesane <tees3359@gmail.com>
2024-11-27 20:58:42 +0900

    feat : add docker script for stop-docker

--- stop-docker.sh
+++ stop-docker.sh
@@ -5,7 +5,10 @@
 
 FILE_PATH="/home/$USER/server"
 
-sudo docker compose -f "$FILE_PATH/back/docker-compose.back.yml" down
 sudo docker compose -f "$FILE_PATH/mq/docker-compose.mq.yml" down
 sudo docker compose -f "$FILE_PATH/database/docker-compose.database.yml" down
 sudo docker compose -f "$FILE_PATH/devops/docker-compose.devops.yml" down
+sudo docker compose -f "$FILE_PATH/logging/docker-compose.logging.yml" down elasticsearch kibana logstash filebeat
+sudo docker compose -f "$FILE_PATH/message_queue/docker-compose.mq.yml" down rabbitmq
+sudo docker compose -f "$FILE_PATH/message_queue/docker-compose.kafka-prod.yml" down
+sudo docker compose -f "$FILE_PATH/back/docker-compose.back.yml" down 
